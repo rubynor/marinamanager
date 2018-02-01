f941d4f33bcaf9b21cdd7259f26c47efbebc7d9f
Ivar Holck <ivar@rubynor.com>
2018-02-01 15:43:12 +0100

    Validate boat, added method for creating boat, changed form text_fields in new boat page to match DB field names

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -5,11 +5,23 @@ class BoatsController < ApplicationController
 	end
 
 	def create
-		render plain: params[:boat].inspect
+		#-render plain: params[:boat].inspect
+		@boat = Boat.new(valid_params)
+		if @boat.save
+			flash[:info] = "The boat was added to the database"
+			redirect_to root_url
+		else
+			flash[:error] = "The boat was NOT added due to an error"
+			render 'new'
+		end
+
 	end
 
 	def show
 
 	end
-
+	private
+	def valid_params
+		params.require(:boat).permit(:reg_nr, :model, :width, :length)
+	end
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,2 +1,6 @@
 class Boat < ApplicationRecord
+    validates :reg_nr, presence: true
+    validates :model, presence: true
+    validates :width,  numericality:{only_integer: true, less_than: 20}
+    validates :length, numericality:{only_integer: true, less_than: 55}
 end
