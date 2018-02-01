14ac2e4c9703b692c11be1fb5bf57694e4c00617
jarnalyrkar <joachimsolstrom@gmail.com>
2018-02-01 09:23:02 +0100

    Add correct route for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-
-
-
-
-
   root 'static_pages#home'
+  get 'home', to: 'static_pages#home'
+  get 'contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
