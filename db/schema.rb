782da95893faa152e132e031db396447b9b90953
jarnalyrkar <joachimsolstrom@gmail.com>
2018-02-06 10:47:50 +0100

    Do fix of phone numbers in DB, not tested

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206070833) do
+ActiveRecord::Schema.define(version: 20180206094351) do
 
   create_table "boats", primary_key: "reg_nr", id: :string, force: :cascade do |t|
     t.string "model"
@@ -18,14 +18,14 @@ ActiveRecord::Schema.define(version: 20180206070833) do
     t.decimal "length"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"   
+    t.integer "user_id"
+    
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "last_name"
     t.string "first_name"
-    t.string "phone", limit: 8
     t.string "bank_account", limit: 11
     t.integer "user_level", limit: 1, default: 0
     t.string "email", default: "", null: false
@@ -39,8 +39,10 @@ ActiveRecord::Schema.define(version: 20180206070833) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "street"
-    t.decimal "house_number"
     t.decimal "zip_code"
+    t.string "phone", limit: 8
+    t.decimal "zip_number"
+    t.string "house_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
