9c69c665bf999db9a9aef9ae6485c5f10142fd9f
Ivar Holck <ivar@rubynor.com>
2018-02-07 13:59:22 +0100

    Berths working

--- app/controllers/berths_controller.rb
+++ app/controllers/berths_controller.rb
@@ -3,11 +3,23 @@ class BerthsController < ApplicationController
     @berth = Berth.new
   end
 
+  def index
+    @berths = Berth.all
+  end
+
+  def show
+    @berth = Berth.find(params[:id])
+  end
+
+  def edit
+    @berth = Berth.find(params[:id])
+  end
+
   def create
     @berth = Berth.new(berth_params)
     if @berth.save
       flash[:info] = "Berth saved"
-      redirect_to root_url
+      redirect_to berths_path
     else
       render 'new'
     end
@@ -21,18 +33,19 @@ class BerthsController < ApplicationController
     @berth = Berth.find(params[:id])
     if @berth.update_attributes(berth_params)
       flash[:success] = "Berth updated"
-      redirect_to root_url
+      redirect_to berths_path
     else
       render 'edit'
     end
   end
   
-  def delete
+  def destroy
 
     #Yes, needs check for admin rights
     @berth = Berth.find(params[:id])
     if @berth.delete
       flash[:success] = "Berth deleted"
+      redirect_to berths_path
     else
       render 'delete'
     end
