f4bd4146ed545aecb296141ace48e84c62bd132a
Ivar Holck <ivar@rubynor.com>
2018-02-08 21:14:56 +0100

    Initial setup for tests. Clean up views/boats

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  
   test "should get dashboard" do
     get user_dashboard_url
     assert_response :success
