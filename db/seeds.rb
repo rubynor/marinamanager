819d175650cc13e97175f29188a69d21f7c072a1
jarnalyrkar <joachimsolstrom@gmail.com>
2018-02-13 23:13:07 +0100

    "Refactor" database, in a sense.
    Renamed, added and removed columns from multiple
    tables. Added seed data for all tables. It works now,
    but will it tomorrow? Time will tell.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User:     user_level, first_name, last_name, email, password, phone_number, street, house_number, zip_code
-# Boat:     reg_nr, model, width, length, user_id
-# Service:  title, description, price_per_month, start_service, end_service
-
-
  User.destroy_all
  User.create!([
-  { user_level: 1, first_name: "Harald", last_name: "Smegma", email: "smegma@gmail.com", password: "passord",
- 	  phone_number: "80088008", street_name: "Krunglevegen", street_number: "33", zip_code: "2300", bank_account: ""},
- 	{ user_level: 0, first_name: "Johnny", last_name: "Johnson", email: "johnny@gmail.com", password: "passord",
- 	  phone_number: "90011001", street_name: "Johnsgate", street_number: "500", zip_code: "2303", bank_account: ""},
- 	{ user_level: 0, first_name: "Preben", last_name: "Smith", email: "prebz@gmail.com", password: "passord",
- 	  phone_number: "47479293", street_name: "Verksvegen", street_number: "3", zip_code: "2285", bank_account: ""}
+  { id: 1, user_level: 1, first_name: "Admin", last_name: "Adminson", email: "admin@gmail.com", password: "passord",
+ 	  phone_number: "80088008", street_name: "Krunglevegen", street_number: "33", post_code: "2300"},
+ 	{ id: 2, user_level: 0, first_name: "Bruk", last_name: "Brukerson", email: "bruker@gmail.com", password: "passord",
+ 	  phone_number: "90011001", street_name: "Johnsgate", street_number: "500", post_code: "2303"},
+ 	{ id: 3, user_level: 0, first_name: "Preben", last_name: "Smith", email: "prebz@gmail.com", password: "passord",
+ 	  phone_number: "47479293", street_name: "Verksvegen", street_number: "3", post_code: "2285"}
                ])
 
  Service.destroy_all
  Service.create!([
-  { title: "Medlemskap", description: "Medlemskontigent", price_per_month: 350, active: true },
-  { title: "Bryggeplass", description: "Prisen er pr. breddemeter", price_per_month: 300, active: true},
-  { title: "Strøm, Opplag", description: "Gjelder fra oktober til april", price_per_month: 600, active: true},
- 	{ title: "Strøm, Brygge", description: "Gjelder fra mai til september", price_per_month: 800, active: false}
+  { id: 1, title: "Medlemskap", description: "Medlemskontigent", price_per_month: 350, active: true },
+  { id: 2, title: "Bryggeplass", description: "Prisen er pr. breddemeter", price_per_month: 300, active: true},
+  { id: 3, title: "Strøm, Opplag", description: "Gjelder fra oktober til april", price_per_month: 600, active: true},
+ 	{ id: 4, title: "Strøm, Brygge", description: "Gjelder fra mai til september", price_per_month: 800, active: false}
                  ])
 
  Pier.destroy_all
@@ -36,26 +31,26 @@
 
  Berth.destroy_all
  Berth.create!([
-		{ pier: Pier.first, berth_number: 1, width: 3, in_service: true },
-		{ pier: Pier.second, berth_number: 2, width: 3, in_service: true }
+		{ pier: Pier.first, berth_number: 1, width: 3, price_per_month: 250, in_service: true },
+		{ pier: Pier.first, berth_number: 2, width: 3, price_per_month: 250, in_service: true }
               ])
 
 Boat.destroy_all
 # Båtdetaljer hentet fra artikkel: https://www.nrk.no/livsstil/slik-velger-du-riktig-bat-1.7069737
 Boat.create!([
-	{ reg_nr: "S99-1", model: "Askeladden C6", width: 2.45, length: 6.25, user_id: 1, berth_number: 1},
-	{ reg_nr: "C33-4", model: "Sting 700 Weekender", width: 2.55, length: 6.99, user_id: 2, berth_number: 2},
-	{ reg_nr: "F23-6", model: "AMT 200 DCC", width: 2.31, length: 6.05, user_id: 3 }
+	{ reg_number: "S99-1", model: "Askeladden C6", width: 2.45, length: 6.25, user_id: 1 },
+	{ reg_number: "C33-4", model: "Sting 700 Weekender", width: 2.55, length: 6.99, user_id: 2 },
+	{ reg_number: "F23-6", model: "AMT 200 DCC", width: 2.31, length: 6.05, user_id: 2 }
              ])
 
-ServiceOrder.destroy_all
+ServiceOrder.delete_all
 ServiceOrder.create!([
-	{ service_id: 1, user_id: 2, start_service: Date.parse("02.12.2018"), end_service: Date.parse("05.12.2018") },
-	{ service_id: 1, user_id: 3, start_service: Date.parse("01.12.2018"), end_service: Date.parse("05.12.2018") },
-	{ service_id: 2, user_id: 2, start_service: Date.parse("02.12.2018"), end_service: Date.parse("05.12.2018") }
+	{ service_id: 1, user_id: 2, start_service: Date.new(2018, 02, 12), end_service: Date.new(2018, 05, 12) }
+	# { service_id: 1, user_id: 3, start_service: "01.12.2018"), end_service: Date("05.12.2018") },
+	# { service_id: 2, user_id: 2, start_service: "02.12.2018"), end_service: Date("05.12.2018") }
                      ])
 
 BerthOrder.destroy_all
 BerthOrder.create!([
-	{ berth_id: 1, boat_id: 2, price_per_month: 250, start_date: "02.12.2018"}
-                   ])
+	{ berth_id: 1, boat_id: 2, start_date: Date.new(2018, 02, 12)}
+                    ])
