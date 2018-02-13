a080512457d3c9f39ca5fab30b6bc769c288b013
jarnalyrkar <joachimsolstrom@gmail.com>
2018-02-13 11:00:23 +0100

    Fix config for smtp delivery

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
-require_relative 'application'
+ require_relative 'application'
 
-config.action_mailer.delivery_method = :smtp
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
