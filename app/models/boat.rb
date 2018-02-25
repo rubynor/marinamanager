b9868f04438c9b8b6299c14f352441d8f6358e9c
jarnalyrkar <joachimsolstrom@gmail.com>
2018-02-25 08:38:28 +0100

    Add links to Marina index
    Add boat overview, and service overview, based on
    which user is logged in.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,6 @@
 class Boat < ApplicationRecord
   belongs_to :user
+  has_many :berth_orders
 
   validates :reg_number, :model, :width, :length, presence: true
   validates :width, :length, numericality: { greater_than_or_equal_to: 1.00 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  has_many :services, through: :service_orders
+  has_many :berths, through: :berth_orders
+  has_many :boats
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :services, through: :service_orders
-  has_many :berths, through: :berth_orders
-  has_many :boats
+
+
 end
