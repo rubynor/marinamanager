0d7b9a8e6a2f3d07c015ae01b0053677158142d5
Ivar Holck <ivar@rubynor.com>
2018-03-01 14:16:37 +0100

    Include capybara in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+require 'capybara/rspec'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
--- spec/views/berth_orders/edit.html.haml_spec.rb
+++ spec/views/berth_orders/edit.html.haml_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "berth_orders/edit", type: :view do
   before(:each) do
-    @berth_order = FactoryBot.create(:berth)
-    @berth_order.save!
+    assign(:berth_orders, [
+      FactoryBot.create(:berth),
+      FactoryBot.create(:berth)
+    ])
   end
+
 
   it "renders the edit berth_order form" do
     render
