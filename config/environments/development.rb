5f692aff7a79ed4bd0f4dc121c6a0c50a55d52b8
jarnalyrkar <joachimsolstrom@gmail.com>
2018-03-05 11:05:53 +0100

    Service Order initial setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:            587,
+    domain:         "domain.of.sender.net",
+    authentication: "plain",
+    user_name:      "dave",
+    password:       "secret",
+    enable_start_tls_auto: true
+  }
 
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
