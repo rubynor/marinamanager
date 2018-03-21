6620283ea6c88df0ab79b8e9a5f8b578b81a48d2
jarnalyrkar <joachimsolstrom@gmail.com>
2018-03-21 10:35:31 +0100

    Add turbolinks to converter calc

--- app/assets/javascripts/boats.js
+++ app/assets/javascripts/boats.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   var input = document.getElementById("calcInput");
   var feetInMeters = 3.2802399;
 
