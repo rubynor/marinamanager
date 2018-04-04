2858e20bb742a726e2012cfa4fd2eb987eb8ee35
jarnalyrkar <joachimsolstrom@gmail.com>
2018-04-04 13:02:51 +0200

    Beginning of js berth-booking

--- app/assets/javascripts/berth_orders.js
+++ app/assets/javascripts/berth_orders.js
@@ -1,13 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-// div-toggle = select-klassen
-// $(document).on('change', '.div-toggle', function() {
-//   var target = $(this).data('target');
-//   var show = $("option:selected", this).data('show');
-//   $(target).children().addClass('hide');
-// });
-//
-// $(document).ready(function() {
-//   $('.div-toggle').trigger('change');
-// });
